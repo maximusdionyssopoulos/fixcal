8cb68bd1bd3442929684c4e033b260d120670551
maximusdionyssopoulos <maximusdionyssopoulos@gmail.com>
2025-02-27 18:36:27 +1100

    update calendar list & calendar page styling

--- app/frontend/pages/Calendar/Index.tsx
+++ app/frontend/pages/Calendar/Index.tsx
@@ -1,6 +1,9 @@
 import { Head, Link } from "@inertiajs/react";
-import Calendar from "./Calendar";
 import { CalendarType } from "./types";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import { ExternalLink, Plus, Tag } from "lucide-react";
+import StatusBadge from "@/components/ui/status-badge";
 
 interface IndexProps {
   calendars: CalendarType[];
@@ -8,23 +11,79 @@ interface IndexProps {
 }
 
 export default function Index({ calendars }: IndexProps) {
+  const getDomain = (url: string) => {
+    try {
+      const domain = new URL(url).hostname.replace("www.", "");
+      return domain;
+    } catch (e) {
+      return url;
+    }
+  };
+
+  const getTeamId = (url: string) => {
+    try {
+      const urlObj = new URL(url);
+      const teamId = urlObj.searchParams.get("teamID");
+      return teamId || "";
+    } catch (e) {
+      return "";
+    }
+  };
   return (
     <>
       <Head title="Calendars" />
 
-      <div>
-        <div className="inline-flex gap-4">
-          <h1>Calendars</h1>
-          <Link href="/">New calendar</Link>
+      <div className="flex flex-col max-w-5xl mx-auto flex-grow relative w-full">
+        <div className="inline-flex gap-4 items-center justify-between">
+          <h1 className="text-3xl">Calendars</h1>
+          <Link
+            href="/"
+            className={cn(buttonVariants({ variant: "outline", size: "lg" }))}
+          >
+            <Plus />
+            New calendar
+          </Link>
         </div>
-        <div>
+        <div className="flex flex-col gap-3 pt-3">
           {calendars.map((calendar) => (
-            <div key={calendar.public_id}>
-              <p>
-                <Link href={`/calendars/${calendar.public_id}`}>
-                  Show this calendar
-                </Link>
-              </p>
+            <div
+              key={calendar.public_id}
+              className="border rounded-lg overflow-hidden shadow-sm hover:shadow-md transition-shadow"
+            >
+              <div className="bg-muted dark:bg-muted p-4 ">
+                <div className="flex flex-wrap gap-3 justify-between">
+                  <Link
+                    href={`/calendars/${calendar.public_id}`}
+                    className="font-medium text-lg flex flex-col gap-0.5"
+                  >
+                    team-{getTeamId(calendar.url)}
+                    <div className="flex items-center text-sm gap-2 text-muted-foreground">
+                      <StatusBadge status="Completed">
+                        {" "}
+                        Matches ({calendar.completed_events.length})
+                      </StatusBadge>
+                      <StatusBadge status="Upcoming">
+                        {" "}
+                        Matches ({calendar.upcoming_events.length})
+                      </StatusBadge>
+                    </div>
+                  </Link>
+                  <a
+                    href={calendar.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-primary hover:text-primary/80 flex flex-col items-center gap-3 text-sm"
+                  >
+                    <span className="inline-flex gap-1 items-center">
+                      View source <ExternalLink className="size-3.5" />
+                    </span>
+                    <span className="text-muted-foreground inline-flex items-center gap-1">
+                      <Tag className="size-4" />
+                      {getDomain(calendar.url)}
+                    </span>
+                  </a>
+                </div>
+              </div>
             </div>
           ))}
         </div>
--- app/frontend/pages/Calendar/Show.tsx
+++ app/frontend/pages/Calendar/Show.tsx
@@ -1,6 +1,9 @@
 import { Head, Link } from "@inertiajs/react";
 import Calendar from "./Calendar";
 import { CalendarType } from "./types";
+import { MoveLeft } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
 
 interface ShowProps {
   calendar: CalendarType;
@@ -10,27 +13,39 @@ export default function Show({ calendar }: ShowProps) {
   return (
     <>
       <Head title={`Calendar #${calendar.public_id}`} />
-      <div className="max-w-5xl p-6">
-        <h1>Calendar #{calendar.public_id}</h1>
-
-        <Calendar calendar={calendar} />
-
-        <div>
-          <Link href={`/calendars/${calendar.public_id}/edit`}>
-            Edit this calendar
-          </Link>
-          <Link href="/calendars">Back to calendars</Link>
-
-          <br />
-
+      <div className="max-w-5xl mx-auto w-full px-6">
+        <div className="flex flex-row gap-6 justify-between pb-2">
           <Link
-            href={`/calendars/${calendar.public_id}`}
-            as="button"
-            method="delete"
+            href="/calendars"
+            className={cn(buttonVariants({ variant: "link", size: "lg" }))}
           >
-            Destroy this calendar
+            <MoveLeft /> All Calendars
           </Link>
+          <div className="space-x-2">
+            <Link
+              href={`/calendars/${calendar.public_id}/edit`}
+              className={cn(buttonVariants({ variant: "outline", size: "lg" }))}
+            >
+              Edit this calendar
+            </Link>
+            {/* TODO: Add a confirmation here */}
+            <Link
+              href={`/calendars/${calendar.public_id}`}
+              as="button"
+              method="delete"
+              className={cn(
+                buttonVariants({
+                  variant: "destructive",
+                  size: "lg",
+                  className: "bg-red-500 hover:bg-red-600 ",
+                }),
+              )}
+            >
+              Destroy this calendar
+            </Link>
+          </div>
         </div>
+        <Calendar calendar={calendar} />
       </div>
     </>
   );
