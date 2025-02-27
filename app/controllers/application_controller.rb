b30850d55a817db89839119a7ceb6d77ed1b55f7
maximusdionyssopoulos <maximusdionyssopoulos@gmail.com>
2025-02-27 18:31:34 +1100

    Add navbar that uses inertia-share to check if a user is signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  inertia_share signed_in: -> { user_signed_in? }
 end
--- app/frontend/components/ui/Layout.tsx
+++ app/frontend/components/ui/Layout.tsx
@@ -1,15 +1,47 @@
-// import { Link } from '@inertiajs/react'
 import { Github } from "lucide-react";
 import { ThemeProvider } from "../theme-provider";
 import ModeToggle from "./mode-toggle";
-import { Head } from "@inertiajs/react";
+import { Head, Link, usePage } from "@inertiajs/react";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const { signed_in } = usePage().props;
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Head title="FixCal" />
       <main className="flex flex-col h-svh">
+        <nav className="z-10 p-4 flex flex-row justify-between">
+          <Link
+            href="/"
+            className="text-3xl font-semibold font-mono backdrop-blur-lg"
+          >
+            <span className="text-orange-600 dark:text-orange-400">Fix</span>cal
+          </Link>
+          {signed_in ? (
+            <div>
+              <Link
+                href="/calendars"
+                className={cn(
+                  buttonVariants({ variant: "link", size: "default" }),
+                )}
+              >
+                Calendars
+              </Link>
+            </div>
+          ) : (
+            <Link
+              href="/auth"
+              className={cn(
+                buttonVariants({ variant: "link", size: "default" }),
+              )}
+            >
+              Login
+            </Link>
+          )}
+        </nav>
         {children}
-        <footer className="p-4 flex flex-row gap-4 items-center backdrop-blur-sm relative z-10">
+        <footer className="p-4 flex flex-row gap-4 items-center relative z-10">
           <ModeToggle />
           <a href="https://github.com/maximusdionyssopoulos">
             <Github size={16} />
