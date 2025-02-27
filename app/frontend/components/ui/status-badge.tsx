d22e310c905e87d37ef309a5796bf91e6af74586
maximusdionyssopoulos <maximusdionyssopoulos@gmail.com>
2025-02-27 18:34:11 +1100

    update colours & add children react node to status badge

--- app/frontend/components/ui/status-badge.tsx
+++ app/frontend/components/ui/status-badge.tsx
@@ -1,13 +1,19 @@
 import { cn } from "@/lib/utils";
 
 const variants: Record<"Upcoming" | "Completed", [classname: string]> = {
-  Upcoming: ["bg-green-50 text-green-700 border-green-200"],
-  Completed: ["bg-orange-50 text-orange-500 border-orange-200 "],
+  Upcoming: [
+    "bg-green-100 text-green-700 border-green-300 dark:bg-green-900 dark:text-green-300 dark:border-green-500",
+  ],
+  Completed: [
+    "bg-orange-100 text-orange-600 border-orange-300  dark:bg-orange-900 dark:text-orange-300 dark:border-orange-500",
+  ],
 };
 export default function StatusBadge({
   status,
+  children,
 }: {
   status: "Upcoming" | "Completed";
+  children: React.ReactNode;
 }) {
   const [variant] = variants[status];
   return (
@@ -18,6 +24,7 @@ export default function StatusBadge({
       )}
     >
       {status}
+      {children}
     </div>
   );
 }
